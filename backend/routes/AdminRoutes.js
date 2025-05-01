3b4a3ddf3dcd4ad7d6cf005c1b0e7353e9c3ce0d
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-01 22:54:35 +0300

    Implemented admin routes with validation middleware

---
+++ backend/routes/AdminRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+import AdminController from '../app/Http/Controllers/AdminController.js'
+import { validateRequest } from '../app/Middlewares/validateRequest.js'
+import { AdminByIdRequest } from '../app/Http/Requests/Admin/AdminByIdRequest.js'
+import { UpdateAdminRequest } from '../app/Http/Requests/Admin/UserUpdateRequest.js'
+
+const router = express.Router()
+
+router.get('/', AdminController.getAllAdmins)
+
+router.get('/:id', AdminByIdRequest, validateRequest, AdminController.getAdminById)
+
+router.put('/:id', UpdateAdminRequest, validateRequest, AdminController.updateAdmin)
+
+router.delete('/:id', AdminByIdRequest, validateRequest, AdminController.deleteAdmin)
+
+export default router
