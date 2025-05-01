dbba02590f01890042dfb395cda49f054f961a8b
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 01:34:45 +0300

    Implemented Character routes with validation and upload middleware

---
+++ backend/routes/CharacterRoutes.js
@@ -0,0 +1,44 @@
+import express from 'express'
+
+import CharacterController from '../app/Http/Controllers/CharacterController.js'
+import { validateRequest } from '../app/Middlewares/validateRequest.js'
+import { AddCharacterRequest } from '../app/Http/Requests/Character/AddCharacterRequest.js'
+import { CharacterByIdRequest } from '../app/Http/Requests/Character/GetCharacterByIdRequest.js'
+import { UpdateCharacterRequest } from '../app/Http/Requests/Character/CharacterUpdateRequest.js'
+import upload from '../app/Middlewares/uploadWithMulter.js'
+
+const router = express.Router()
+
+router.get('/', CharacterController.getAllCharacters)
+
+router.get(
+  '/:id',
+  CharacterByIdRequest,
+  validateRequest,
+  CharacterController.getCharacterById
+)
+
+router.post(
+  '/',
+  upload.single('image_url'),
+  AddCharacterRequest,
+  validateRequest,
+  CharacterController.addCharacter
+)
+
+router.put(
+  '/:id',
+  upload.single('image_url'),
+  UpdateCharacterRequest,
+  validateRequest,
+  CharacterController.updateCharacter
+)
+
+router.delete(
+  '/:id',
+  CharacterByIdRequest,
+  validateRequest,
+  CharacterController.deleteCharacter
+)
+
+export default router
