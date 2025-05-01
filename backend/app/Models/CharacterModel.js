1efa2099d87297282fb86a2b82ed66d8c1b65c97
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 00:56:17 +0300

    Implemented character model with sequelize validations

---
+++ backend/app/Models/CharacterModel.js
@@ -0,0 +1,52 @@
+import sequelize from '../../config/Connection.js'
+import { DataTypes } from 'sequelize'
+
+const Character = sequelize.define(
+  'Character',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'users',
+        key: 'id',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      allowNull: true,
+      unique: 'unique_user_character',
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: 'unique_user_character',
+      validate: {
+        notNull: { msg: 'Name is required' },
+        notEmpty: { msg: 'Name must not be empty' },
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'description is required' },
+        notEmpty: { msg: 'description must not be empty' },
+      },
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'Image is required' },
+        notEmpty: { msg: 'Image must not be empty' },
+      },
+    },
+  },
+  { tableName: 'characters' }
+)
+
+export default Character
