fd04fe966cdf6a7a48e8346319605388e4112ae0
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-01 23:54:22 +0300

    Added User Plan relation to models index

--- backend/app/Models/index.js
+++ backend/app/Models/index.js
@@ -1,4 +1,8 @@
 import User from './UserModel.js'
 import Admin from './AdminModel.js'
+import Plan from './PlanModel.js'
 
-export { Admin, User }
+Plan.hasMany(User, { foreignKey: 'plan_id' })
+User.belongsTo(Plan, { foreignKey: 'plan_id' })
+
+export { Admin, User, Plan }
