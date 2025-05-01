3488fb27f8b18cb9a7a3a1cfbb532d378dc5d11a
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-01 21:17:35 +0300

    Finished user model with sequelize model validation

---
+++ backend/app/Models/UserModel.js
@@ -0,0 +1,64 @@
+import sequelize from '../../config/Connection.js'
+import { DataTypes } from 'sequelize'
+
+const User = sequelize.define(
+  'User',
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: { msg: 'Must be a valid email' },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'Password is required' },
+        notEmpty: { msg: 'Password must not be empty' },
+        len: {
+          args: [6],
+          msg: 'Password must be at least 6 characters long',
+        },
+      },
+    },
+
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'First name is required' },
+        notEmpty: { msg: 'First name must not be empty' },
+      },
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'Last name is required' },
+        notEmpty: { msg: 'Last name must not be empty' },
+      },
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: {
+          args: [8],
+          msg: 'phone must be at least 8 characters long',
+        },
+      },
+    },
+
+    profile_picture: { type: DataTypes.STRING, allowNull: true },
+    credits: { type: DataTypes.INTEGER, defaultValue: 0 },
+  },
+  {
+    tableName: 'users',
+  }
+)
+
+export default User
