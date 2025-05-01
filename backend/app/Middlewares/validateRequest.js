8a60d7e586e3bcf0081273ce80d9648aa656d383
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-01 21:07:22 +0300

    Added a validate request middleware

---
+++ backend/app/Middlewares/validateRequest.js
@@ -0,0 +1,22 @@
+import { validationResult } from 'express-validator'
+import ResponseTrait from '../Traits/ResponseTrait.js'
+
+export function validateRequest(req, res, next) {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    
+    //errors.array().map to show only needed attributes and hide others
+    return ResponseTrait.response(
+      res,
+      false,
+      'Request validation error',
+      errors.array().map((err) => ({
+        type: err.type,
+        msg: err.msg,
+      })),
+      422
+    )
+  }
+  next()
+}
