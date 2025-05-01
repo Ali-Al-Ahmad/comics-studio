8706f18beec4cb21b35e919e6213774ca4c25b6e
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 01:11:11 +0300

    Implemented CharacterController deleteCharacter function

--- backend/app/Http/Controllers/CharacterController.js
+++ backend/app/Http/Controllers/CharacterController.js
@@ -58,6 +58,25 @@ class CharacterController extends Controller {
       )
     }
   }
+
+  static async deleteCharacter(req, res) {
+    try {
+      const result = await CharacterService.delete(req.params.id)
+      return CharacterController.returnResponse(
+        res,
+        result.success,
+        result.message,
+        result.data
+      )
+    } catch (error) {
+      return CharacterController.returnResponse(
+        res,
+        false,
+        'Error in Delete Character Controller',
+        error
+      )
+    }
+  }
 }
 
 export default CharacterController
