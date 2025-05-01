7f6f1471a1fdd0ec663356caee56bcc68cae0203
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-01 20:55:40 +0300

    Added middleware to log request body

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
+import logReuestBodyData from './app/Middlewares/RequestBodyData'
+
 dotenv.config()
 
 const app = express()
@@ -11,6 +13,8 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('./'))
+
+app.use(logReuestBodyData)
 
 //Start server
 const PORT = process.env.PORT || 4000
