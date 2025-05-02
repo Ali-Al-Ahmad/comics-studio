0d49c5df0790f4975deabbbf32a646e9b7149182
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 03:10:31 +0300

    Added User Book relation to models index

--- backend/app/Models/index.js
+++ backend/app/Models/index.js
@@ -2,6 +2,7 @@ import User from './UserModel.js'
 import Admin from './AdminModel.js'
 import Plan from './PlanModel.js'
 import Character from './CharacterModel.js'
+import Book from './BookModel.js'
 
 Plan.hasMany(User, { foreignKey: 'plan_id' })
 User.belongsTo(Plan, { foreignKey: 'plan_id' })
@@ -9,4 +10,7 @@ User.belongsTo(Plan, { foreignKey: 'plan_id' })
 User.hasMany(Character, { foreignKey: 'user_id' })
 Character.belongsTo(User, { foreignKey: 'user_id' })
 
-export { Admin, User, Plan, Character }
+User.hasMany(Book, { foreignKey: 'user_id' })
+Book.belongsTo(User, { foreignKey: 'user_id' })
+
+export { Admin, User, Plan, Character, Book }
