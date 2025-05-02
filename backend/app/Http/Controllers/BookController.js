a4ad5c8af7f77d21480ebc74b9edc73d87c0f3a0
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 03:23:57 +0300

    Implemented BookController deleteBook function

--- backend/app/Http/Controllers/BookController.js
+++ backend/app/Http/Controllers/BookController.js
@@ -58,6 +58,25 @@ class BookController extends Controller {
       )
     }
   }
+
+  static async deleteBook(req, res) {
+    try {
+      const result = await BookService.delete(req.params.id)
+      return BookController.returnResponse(
+        res,
+        result.success,
+        result.message,
+        result.data
+      )
+    } catch (error) {
+      return BookController.returnResponse(
+        res,
+        false,
+        'Error in Delete Book Controller',
+        error
+      )
+    }
+  }
 }
 
 export default BookController
