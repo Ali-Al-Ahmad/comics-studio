bf6237c4ccab89cd2d99c23e892651c87086372b
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 04:16:39 +0300

    Implemnted ComicController getComicById function

--- backend/app/Http/Controllers/ComicController.js
+++ backend/app/Http/Controllers/ComicController.js
@@ -20,6 +20,25 @@ class ComicController extends Controller {
       )
     }
   }
+
+  static async getComicById(req, res) {
+    try {
+      const result = await ComicService.byId(req.params.id)
+      return ComicController.returnResponse(
+        res,
+        result.success,
+        result.message,
+        result.data
+      )
+    } catch (error) {
+      return ComicController.returnResponse(
+        res,
+        false,
+        'Error in Comic By Id Controller',
+        error
+      )
+    }
+  }
 }
 
 export default ComicController
