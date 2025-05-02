eea71187215b69ddfe9af8f2d1be245a8f8eed67
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 04:15:44 +0300

    Implemnted ComicController getAllComics function

---
+++ backend/app/Http/Controllers/ComicController.js
@@ -0,0 +1,25 @@
+import Controller from './Controller.js'
+import ComicService from '../../Services/ComicService.js'
+
+class ComicController extends Controller {
+  static async getAllComics(req, res) {
+    try {
+      const result = await ComicService.all()
+      return ComicController.returnResponse(
+        res,
+        result.success,
+        result.message,
+        result.data
+      )
+    } catch (error) {
+      return ComicController.returnResponse(
+        res,
+        false,
+        'Error in All Comic Controller',
+        error
+      )
+    }
+  }
+}
+
+export default ComicController
