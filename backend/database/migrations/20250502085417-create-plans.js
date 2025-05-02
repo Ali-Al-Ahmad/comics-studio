f29540943adfcce5b283e855cbfda0044b427077
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 11:57:30 +0300

    Created plans migration

---
+++ backend/database/migrations/20250502085417-create-plans.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('plans', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      credits: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('plans')
+  },
+}
