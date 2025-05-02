cc852ef03dca7d1e61c26520864469a108f714ff
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 12:23:42 +0300

    Modified service class with hashPassword trait

--- backend/app/Traits/HashPasswordTrait.js
+++ backend/app/Traits/HashPasswordTrait.js
@@ -4,7 +4,7 @@ class HashPasswordTrait {
   static async hashPassword(pass) {
     try {
       const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS))
-      const hashedPassword = await bcrypt.hash(password, salt)
+      const hashedPassword = await bcrypt.hash(pass, salt)
       return hashedPassword
     } catch (error) {
       throw error
