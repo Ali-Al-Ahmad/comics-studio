42726049fc56a103154ba102ea5a5bc33bb9eb5b
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 03:21:08 +0300

    Implemented BookController getAllBooks function

---
+++ backend/app/Http/Controllers/BookController.js
@@ -0,0 +1,25 @@
+import Controller from './Controller.js'
+import BookService from '../../Services/BookService.js'
+
+class BookController extends Controller {
+  static async getAllBooks(req, res) {
+    try {
+      const result = await BookService.all()
+      return BookController.returnResponse(
+        res,
+        result.success,
+        result.message,
+        result.data
+      )
+    } catch (error) {
+      return BookController.returnResponse(
+        res,
+        false,
+        'Error in All Book Controller',
+        error
+      )
+    }
+  }
+}
+
+export default BookController
