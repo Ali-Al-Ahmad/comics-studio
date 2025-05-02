7e9f504a7c2772c446dc3b149df8dc1150978859
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-02 03:22:12 +0300

    Implemented BookController getBookById function

--- backend/app/Http/Controllers/BookController.js
+++ backend/app/Http/Controllers/BookController.js
@@ -20,6 +20,25 @@ class BookController extends Controller {
       )
     }
   }
+
+  static async getBookById(req, res) {
+    try {
+      const result = await BookService.byId(req.params.id)
+      return BookController.returnResponse(
+        res,
+        result.success,
+        result.message,
+        result.data
+      )
+    } catch (error) {
+      return BookController.returnResponse(
+        res,
+        false,
+        'Error in Book By Id Controller',
+        error
+      )
+    }
+  }
 }
 
 export default BookController
