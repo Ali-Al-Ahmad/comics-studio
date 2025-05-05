023593941e2fddd8a9a7bdbc87bee7661ac90653
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-06 01:31:38 +0300

    Implemented CharacterTest createAndLoginUser function

---
+++ backend/tests/Feature/Character.test.js
@@ -0,0 +1,26 @@
+import request from 'supertest'
+import app from '../../server.js'
+import { faker } from '@faker-js/faker'
+import { createUser } from '../../database/factories/UserFactory.js'
+import { Character } from '../../app/Models/index.js'
+import sequelize from '../../config/Connection.js'
+import path from 'path'
+import fs from 'fs/promises'
+
+describe('Character Controller Tests (User)', () => {
+  let userToken
+  let userId
+
+  const createAndLoginUser = async () => {
+    const data = await createUser()
+    const res = await request(app).post('/api/v1/auth/user/login').send({
+      email: data.user.email,
+      password: data.plainPassword,
+    })
+
+    return {
+      token: res.body.data.token,
+      userId: data.user.id,
+    }
+  }
+})
