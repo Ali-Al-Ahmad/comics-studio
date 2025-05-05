7de69878d74a78909bd20da37d6fa60625f6203e
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-05 23:25:09 +0300

    Implemented AdminTest createAndLoginAdmin funtion

---
+++ backend/tests/Feature/Admin.test.js
@@ -0,0 +1,28 @@
+import request from 'supertest'
+import app from '../../server.js'
+import { faker } from '@faker-js/faker'
+import { createAdmin } from '../../database/factories/AdminFactory.js'
+import { Admin } from '../../app/Models/index.js'
+import sequelize from '../../config/Connection.js'
+
+describe('Admin Controller Tests', () => {
+  let adminToken
+  let adminId
+
+  const createAndLoginAdmin = async () => {
+    const data = await createAdmin()
+    const res = await request(app).post('/api/v1/auth/admin/login').send({
+      email: data.admin.email,
+      password: data.plainPassword,
+    })
+
+    if (!res.body.success) {
+      throw new Error('Admin login failed during test setup.')
+    }
+
+    return {
+      token: res.body.data.token,
+      admin: data.admin,
+    }
+  }
+})
