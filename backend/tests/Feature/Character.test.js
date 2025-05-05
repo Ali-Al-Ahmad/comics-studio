86037ade143314a30d043deb13a215dd6a16a1df
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-06 01:32:26 +0300

    Implemented CharacterTest truncateAllTables function

--- backend/tests/Feature/Character.test.js
+++ backend/tests/Feature/Character.test.js
@@ -23,4 +23,13 @@ describe('Character Controller Tests (User)', () => {
       userId: data.user.id,
     }
   }
+
+  const truncateAllTables = async () => {
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 0', { raw: true })
+    for (const model of Object.values(sequelize.models)) {
+      await model.destroy({ where: {}, truncate: true, force: true })
+    }
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 1', { raw: true })
+  }
+  
 })
