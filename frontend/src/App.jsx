e36fc315f975af177b23e263651b2fd8ec8cd0c7
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-06 16:59:35 +0300

    Added register route to app for test api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css'
+import { Route, Routes } from 'react-router'
+import Register from './pages/register/Register'
+import Home from './pages/Home/Home'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        ></Route>
+
+        <Route
+          path='/register'
+          element={<Register />}
+        ></Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
