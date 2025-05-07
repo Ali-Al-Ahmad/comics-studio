5a74a772cf9b2579b1ecf3b61515500d459a6dca
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-07 15:56:29 +0300

    Added persist library to redux to persist state

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from 'redux'
+
 import userReducer from './slices/userSlice'
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const rootReducer = combineReducers({
+  user: userReducer,
 })
 
-export default store
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: {
+    user: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
+
