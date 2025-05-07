f20116cbd48a780a2ee5daa7080dae2511f6aac1
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-07 16:13:04 +0300

    Fixed redux user slice

--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -1,22 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {
-  user: null,
-}
+const initialState = null
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     login(state, action) {
-      state.user = action.payload
+      return action.payload
     },
-    logout(state) {
-      state.user = null
+    logout() {
+      return null
     },
     updateProfile(state, action) {
-      if (state.user) {
-        state.user = { ...state.user, ...action.payload }
+      return {
+        ...state,
+        ...action.payload,
       }
     },
   },
