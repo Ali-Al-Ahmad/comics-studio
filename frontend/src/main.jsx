3e6fb222789759fcacd3f207d5ac447649836dfd
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-08 11:18:28 +0300

    Added toast customized component to the main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import Toast from './components/Toast/Toast'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       persistor={persistor}
     >
       <BrowserRouter>
+        <Toast />
         <App />
       </BrowserRouter>
     </PersistGate>
