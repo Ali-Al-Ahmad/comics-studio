49fcdecbfca18d5f43c3a05a49b63fc2aa2c8595
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-08 11:23:09 +0300

    Added redux toast dispatch functions to the toast

--- frontend/src/components/Toast/Toast.jsx
+++ frontend/src/components/Toast/Toast.jsx
@@ -7,6 +7,27 @@ import ErrorIcon from '../../assets/icons/circle-xmark-solid.svg'
 import InfoIcon from '../../assets/icons/circle-info-solid.svg'
 
 const Toast = () => {
+  const { show, type, message } = useSelector((state) => state.toast)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    let timer
+    if (show) {
+      timer = setTimeout(() => {
+        dispatch(hideToast())
+      }, 4000)
+    }
+    return () => {
+      if (timer) clearTimeout(timer)
+    }
+  }, [show, dispatch])
+
+  if (!show) return null
+
+  const handleClose = () => {
+    dispatch(hideToast())
+  }
+
   return <div className='toast-container'></div>
 }
 
