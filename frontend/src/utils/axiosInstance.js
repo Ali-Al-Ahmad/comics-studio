3a071c890637ad726ab61a04c01f9c8206ec331f
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-13 02:17:56 +0300

    Edited axiosInstance base url for testing

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1',
+  baseURL: 'http://3.79.63.29:4000/api/v1',
 })
 
 axiosInstance.interceptors.request.use(
