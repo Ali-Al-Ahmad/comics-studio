6c9f6b8fe7f58d0d2830c145426176609ebd91fc
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-12 23:47:22 +0300

    Created gallery page with states

--- frontend/src/pages/Gallery/Gallery.jsx
+++ frontend/src/pages/Gallery/Gallery.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react'
 import './Gallery.css'
+import axiosInstance from '../../utils/axiosInstance'
+import Spinner from '../../components/Spinner/Spinner'
+import { useDispatch, useSelector } from 'react-redux'
+import { showToast } from '../../redux/slices/toastSlice'
+import GalleryHeader from '../../components/Gallery/GalleryHeader/GalleryHeader'
+import SearchBox from '../../components/Gallery/SearchBox/SearchBox'
+import ComicCard from '../../components/Gallery/ComicCard/ComicCard'
+import { fakeComics } from '../../fakeData/comicsData'
 
 const Gallery = () => {
-  return <div>Gallery</div>
+  const [comics, setComics] = useState([])
+  const [filteredComics, setFilteredComics] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [activeFilter, setActiveFilter] = useState('all')
+  const [searchTerm, setSearchTerm] = useState('')
+  const dispatch = useDispatch()
+  const { isCollapsed } = useSelector((state) => state.sidebar)
+  const { comicIds: recentlyViewedIds } = useSelector(
+    (state) => state.recentlyViewed
+  )
+
+  return (
+    <div
+      className={`gallery-container ${
+        isCollapsed ? 'sidebar-collapsed' : 'sidebar-expanded'
+      }`}
+    ></div>
+  )
 }
 
 export default Gallery
