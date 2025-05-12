ea30233be887f3bfe823afaf5b6d1128803f7cd7
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-12 12:47:28 +0300

    Implemented landing page hero section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -252,7 +252,51 @@ const Home = () => {
           </button>
         </div>
       </nav>
- 
+      <section className='hero-section'>
+        <div className='hero-content'>
+          <h1 className='hero-title'>
+            AI <span>Comics</span> Studio
+          </h1>
+          <p className='hero-subtitle'>
+            Create Stunning Comics without Drawing Skills using our cutting-edge
+            AI Comic Generator. Bring Your Comic Dreams to Life with AI
+            Creativity.
+          </p>
+          <div className='hero-buttons'>
+            <Link
+              to='/register'
+              className='btn btn-primary'
+            >
+              Get Started for Free
+            </Link>
+            <a
+              href='#features-overview'
+              className='learn-more-link'
+            >
+              Learn more
+              <Icon
+                icon='pajamas:long-arrow'
+                className='learn-more-arrow'
+              />
+            </a>
+          </div>
+          <div className='animated-text-wrapper'>
+            <ReactTyped
+              strings={[
+                'Unleash Your Creative Storytelling',
+                'Create Without Drawing Skills',
+                'Build Comics in Minutes',
+                'Let AI Power Your Stories',
+              ]}
+              typeSpeed={50}
+              backSpeed={30}
+              backDelay={2000}
+              loop
+              className='animated-typed-text'
+            />
+          </div>
+        </div>
+      </section>
 
 
 
