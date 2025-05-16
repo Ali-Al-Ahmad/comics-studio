c4c25616db08eca225e30c0b5bb9606d82654325
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-16 14:12:36 +0300

    Edited ComicService generate replicate comic function

--- backend/app/Http/Requests/Comic/GenerateComicsRequest.js
+++ backend/app/Http/Requests/Comic/GenerateComicsRequest.js
@@ -2,10 +2,4 @@ import { body } from 'express-validator'
 
 export const GenerateComicRequest = [
   body('user_prompt').notEmpty().withMessage('User prompt is required'),
-  body('character_image').custom((value, { req }) => {
-    if (!req.file) {
-      throw new Error('Character image file is required')
-    }
-    return true
-  }),
 ]
