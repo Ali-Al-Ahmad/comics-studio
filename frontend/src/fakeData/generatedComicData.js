565d942928c84bcef717ecd7c532f175b2e7df21
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-16 14:14:08 +0300

    Added fake generated comic data to test on

---
+++ frontend/src/fakeData/generatedComicData.js
@@ -0,0 +1,76 @@
+const fakeGeneratedComicData = {
+  success: true,
+  message: 'Generated comic data',
+  data: {
+    user_id: 19,
+    book: {
+      is_public: false,
+      id: 22,
+      title: 'a man want to be a doctor',
+      user_id: 19,
+      image_url:
+        'https://replicate.delivery/xezq/vpvWW9qDhF5AJFc1ooz7NlzZdXelcGtFDRfxkSTQtCzwU5sUA/unique_6.png',
+      character_id: '10',
+      updatedAt: '2025-05-15T12:50:56.385Z',
+      createdAt: '2025-05-15T12:50:56.385Z',
+    },
+    comics: [
+      {
+        id: 90,
+        caption: 'Dreams take flight.',
+        image_url:
+          'https://replicate.delivery/xezq/lksgUYoMHULfByCdIjTsobwRhYOIPhmiBzCrSeiQF7lvU5sUA/unique_0.png',
+        book_id: 22,
+        updatedAt: '2025-05-15T12:50:56.435Z',
+        createdAt: '2025-05-15T12:50:56.435Z',
+      },
+      {
+        id: 91,
+        caption: 'Relentless determination.',
+        image_url:
+          'https://replicate.delivery/xezq/qDfXfZFxwsmeeSODlbLPfZMPLhGfkE7BkFQSe4ZB6yXmXqcWKA/unique_1.png',
+        book_id: 22,
+        updatedAt: '2025-05-15T12:50:56.438Z',
+        createdAt: '2025-05-15T12:50:56.438Z',
+      },
+      {
+        id: 92,
+        caption: 'The big test begins.',
+        image_url:
+          'https://replicate.delivery/xezq/ybbPQObg1j6XElxdzhOPeeaAAqknecVxtIYgeRKfeR30LVOLF/unique_2.png',
+        book_id: 22,
+        updatedAt: '2025-05-15T12:50:56.439Z',
+        createdAt: '2025-05-15T12:50:56.439Z',
+      },
+      {
+        id: 93,
+        caption: 'Pressure mounts.',
+        image_url:
+          'https://replicate.delivery/xezq/v7Db6Ty9leRmWyEBRl9qyxdPsyqH7PYJ7nKSu5yxZ36XqcWKA/unique_3.png',
+        book_id: 22,
+        updatedAt: '2025-05-15T12:50:56.440Z',
+        createdAt: '2025-05-15T12:50:56.440Z',
+      },
+      {
+        id: 94,
+        caption: 'Victory achieved.',
+        image_url:
+          'https://replicate.delivery/xezq/i8TCOJ4u9iZiOZuWWbVpmT0Zerkt2ex4gGVyiPefyWRflKnlC/unique_4.png',
+        book_id: 22,
+        updatedAt: '2025-05-15T12:50:56.442Z',
+        createdAt: '2025-05-15T12:50:56.442Z',
+      },
+      {
+        id: 95,
+        caption: 'A doctor’s journey begins.',
+        image_url:
+          'https://replicate.delivery/xezq/FRcD8OPBuvI3HxDFqrT0Ma0lTUZPOZ9y4fwECkPICgnXqcWKA/unique_5.png',
+        book_id: 22,
+        updatedAt: '2025-05-15T12:50:56.443Z',
+        createdAt: '2025-05-15T12:50:56.443Z',
+      },
+    ],
+  },
+}
+
+export default fakeGeneratedComicData;
