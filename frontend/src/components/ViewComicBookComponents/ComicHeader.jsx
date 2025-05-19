771ef7bdf001bccaa718220318a12486008dc2cc
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-19 18:36:05 +0300

    Added ComicHeader component for displaying comic header information

---
+++ frontend/src/components/ViewComicBookComponents/ComicHeader.jsx
@@ -0,0 +1,9 @@
+import { Icon } from '@iconify-icon/react'
+import PropTypes from 'prop-types'
+import './ComicHeader.css'
+
+const ComicHeader = ({ user, onBack }) => {
+  return <div className='view-comic-header'></div>
+}
+
+export default ComicHeader
