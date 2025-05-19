f344efd3049bdb73c3c98b0e8bf4fb77811bb453
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-19 18:17:06 +0300

    Refactored ViewComicBook component to implement state management and improve rendering logic

--- frontend/src/pages/ViewComicBook/ViewComicBook.jsx
+++ frontend/src/pages/ViewComicBook/ViewComicBook.jsx
@@ -1,7 +1,32 @@
+import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import ComicHeader from '../../components/ViewComicBookComponents/ComicHeader'
+import LoadingState from '../../components/ViewComicBookComponents/LoadingState'
+import ComicContent from '../../components/ViewComicBookComponents/ComicContent'
+import { useComicBook } from '../../hooks/useComicBook'
 import './ViewComicBook.css'
 
 const ViewComicBook = () => {
-  return <div>ViewComicBook</div>
+  const { bookId } = useParams()
+  const dispatch = useDispatch()
+  const { isCollapsed } = useSelector((state) => state.sidebar)
+  const user = useSelector((state) => state.user)
+  const {
+    comic,
+    loading,
+    handleChangeViewMode,
+    handleEditPanel,
+    handleRegeneratePanel,
+    handleCaptionChange,
+    handlePublishStatusChange,
+  } = useComicBook(bookId, dispatch)
+  return (
+    <div
+      className={`view-comic-container ${
+        isCollapsed ? 'sidebar-collapsed' : 'sidebar-expanded'
+      }`}
+    ></div>
+  )
 }
 
 export default ViewComicBook
