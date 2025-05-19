7bd0453f89b2f9d3552bbc946bf6da5bf24436f2
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-19 18:38:09 +0300

    Added LoadingState component with initial structure and imports

---
+++ frontend/src/components/ViewComicBookComponents/LoadingState.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+import Spinner from '../Spinner/Spinner'
+import './LoadingState.css'
+
+const LoadingState = ({ message }) => {
+  return (
+
+  )
+}
+
+export default LoadingState
