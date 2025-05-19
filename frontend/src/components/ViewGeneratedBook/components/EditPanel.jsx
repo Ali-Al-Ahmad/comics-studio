8edd0306fe015b6ebf69360bb7319517c84f351b
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-19 17:58:44 +0300

    Added EditPanel component for editing comic book panels

---
+++ frontend/src/components/ViewGeneratedBook/components/EditPanel.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Icon } from '@iconify-icon/react'
+import Spinner from '../../Spinner/Spinner'
+import './EditPanel.css'
+
+const EditPanel = ({
+  panel,
+  panelIndex,
+  loading,
+  onCancelEdit,
+  onRegeneratePanel,
+  onCaptionChange,
+}) => {
+  return <div className='edit-panel-container'></div>
+}
+
+export default EditPanel
