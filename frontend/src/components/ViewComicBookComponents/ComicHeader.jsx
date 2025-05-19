45aba5d182c4ea95f8c000588869a62486e69ef7
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-19 18:36:15 +0300

    Implemented ComicHeader component with navigation and user credits display

--- frontend/src/components/ViewComicBookComponents/ComicHeader.jsx
+++ frontend/src/components/ViewComicBookComponents/ComicHeader.jsx
@@ -3,7 +3,29 @@ import PropTypes from 'prop-types'
 import './ComicHeader.css'
 
 const ComicHeader = ({ user, onBack }) => {
-  return <div className='view-comic-header'></div>
+  return (
+    <div className='view-comic-header'>
+      <div className='view-comic-navigation'>
+        <button
+          className='back-button'
+          onClick={onBack}
+        >
+          <Icon
+            icon='mdi:arrow-left'
+            width='20'
+            height='20'
+          />
+          All comics
+        </button>
+      </div>
+      {user && (
+        <div className='credits-display'>
+          <span className='credits-value'>{user.credits || 50} Credits</span>
+        </div>
+      )}
+    </div>
+  )
 }
+
 
 export default ComicHeader
