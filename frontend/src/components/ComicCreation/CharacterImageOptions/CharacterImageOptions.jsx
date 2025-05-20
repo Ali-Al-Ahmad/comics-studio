73cbf073740423e6dc04fa7ef1140277cb60b176
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:12:04 +0300

    Added CharacterImageOptions component with initial structure

---
+++ frontend/src/components/ComicCreation/CharacterImageOptions/CharacterImageOptions.jsx
@@ -0,0 +1,28 @@
+import { useCallback } from 'react'
+import CharacterSelector from '../CharacterSelector/CharacterSelector'
+import ImageUploader from '../ImageUploader/ImageUploader'
+import './CharacterImageOptions.css'
+import PropTypes from 'prop-types'
+
+const CharacterImageOptions = ({
+  characters,
+  selectedCharacter,
+  onCharacterSelect,
+  loadingCharacters,
+  uploadedImage,
+  setUploadedImage,
+  loading,
+  isGenerating,
+}) => {
+
+  )
+
+  return (
+    <div className='character-image-options-container'>
+
+
+    </div>
+  )
+}
+
+export default CharacterImageOptions
