4b665470ccd5c2464886e3f569b2525f6c2d5a31
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:45:01 +0300

    Refactored CreateComic component by adding necessary imports and updating the return structure for improved functionality

--- frontend/src/pages/CreateComic/CreateComic.jsx
+++ frontend/src/pages/CreateComic/CreateComic.jsx
@@ -1,7 +1,28 @@
-import './CreateComic.css'
+import { useState, useEffect, useRef } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { showToast } from '../../redux/slices/toastSlice'
+import axiosInstance from '../../utils/axiosInstance'
+import ViewGeneratedBook from '../../components/ViewGeneratedBook/ViewGeneratedBook'
+import {
+  CreateComicHeader,
+  PromptSection,
+} from '../../components/ComicCreation'
+import './css/base.css'
+import './css/prompt-form.css'
+import './css/character-selection.css'
+import './css/image-upload.css'
+import './css/panels.css'
 
 const CreateComic = () => {
-  return <div>CreateComic</div>
+  return (
+    <div
+      className={`create-comic-container ${
+        isCollapsed ? 'sidebar-collapsed' : 'sidebar-expanded'
+      }`}
+    >
+      {' '}
+    </div>
+  )
 }
 
 export default CreateComic
