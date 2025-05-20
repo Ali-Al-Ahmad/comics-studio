0b6cf4219b15ad876f52e0ee1136bfc5151ecd3f
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:22:48 +0300

    Added styles array to ComicForm component for style selection

--- frontend/src/components/ComicCreation/ComicForm/ComicForm.jsx
+++ frontend/src/components/ComicCreation/ComicForm/ComicForm.jsx
@@ -2,7 +2,22 @@ import PropTypes from 'prop-types'
 import './ComicForm.css'
 
 const ComicForm = ({ comic, loading, onPromptChange, onStyleChange }) => {
-  return <div className='prompt-input-container'></div>
+  const styles = [
+    { value: 'No style', label: '(No style)' },
+    { value: 'Japanese Anime', label: 'Japanese Anime' },
+    { value: 'Cinematic', label: 'Cinematic' },
+    { value: 'Disney Character', label: 'Disney Character' },
+    { value: 'Photographic', label: 'Photographic' },
+    { value: 'Comic book', label: 'Comic book' },
+    { value: 'Line art', label: 'Line art' },
+  ]
+
+  return (
+    <div className='prompt-input-container'>
+
+    </div>
+  )
 }
+
 
 export default ComicForm
