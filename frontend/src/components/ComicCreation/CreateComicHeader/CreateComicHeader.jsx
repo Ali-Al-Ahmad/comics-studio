7de3e74eebfbed210592357a9233386e6403a835
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:24:04 +0300

    Added CreateComicHeader component with credits display

--- frontend/src/components/ComicCreation/CreateComicHeader/CreateComicHeader.jsx
+++ frontend/src/components/ComicCreation/CreateComicHeader/CreateComicHeader.jsx
@@ -2,7 +2,16 @@ import PropTypes from 'prop-types'
 import './CreateComicHeader.css'
 
 const CreateComicHeader = ({ credits }) => {
-  return <div className='create-comic-header'></div>
+  return (
+    <div className='create-comic-header'>
+      <h1>Create Comic</h1>
+      <div className='credits-display'>
+        <span className='credits-value'>{credits} Credits</span>
+      </div>
+    </div>
+  )
 }
+
+
 
 export default CreateComicHeader
