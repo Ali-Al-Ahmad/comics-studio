cf8cb6c6c5d789cfc6cd4f245ae7cc4bb3b17a98
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:34:35 +0300

    Added PromptSection component for comic creation

---
+++ frontend/src/components/ComicCreation/PromptSection/PromptSection.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+import ComicForm from '../ComicForm/ComicForm'
+import CharacterImageOptions from '../CharacterImageOptions/CharacterImageOptions'
+import GenerateButton from '../GenerateButton/GenerateButton'
+import './PromptSection.css'
+
+const PromptSection = ({
+  comic,
+  loading,
+  characters,
+  selectedCharacter,
+  loadingCharacters,
+  uploadedImage,
+  onPromptChange,
+  onStyleChange,
+  onCharacterSelect,
+  setUploadedImage,
+  onGenerate,
+}) => {
+  return (
+    <div className='prompt-section'>
+
+
+
+    </div>
+  )
+}
+
+
+
+export default PromptSection
