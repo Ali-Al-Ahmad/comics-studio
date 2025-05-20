196a7de5a434ee1a5c7c29ab7f665c977bdbc438
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:45:14 +0300

    Refactored CreateComic component by organizing state variables and improving code structure for better readability

--- frontend/src/pages/CreateComic/CreateComic.jsx
+++ frontend/src/pages/CreateComic/CreateComic.jsx
@@ -14,6 +14,35 @@ import './css/image-upload.css'
 import './css/panels.css'
 
 const CreateComic = () => {
+  const [comic, setComic] = useState({
+    prompt: '',
+    style: 'Comic book',
+    panels: [],
+    captions: [],
+    viewMode: 'grid',
+    isGenerating: false,
+    isEditing: false,
+    currentEditPanel: null,
+  })
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  const { isCollapsed } = useSelector((state) => state.sidebar)
+  const user = useSelector((state) => state.user)
+  const credits = user?.credits || 0
+
+  const comicViewRef = useRef(null)
+
+  const [characters, setCharacters] = useState([])
+  const [selectedCharacter, setSelectedCharacter] = useState(null)
+  const [loadingCharacters, setLoadingCharacters] = useState(false)
+
+  const [uploadedImage, setUploadedImage] = useState(null) 
+
+
+
+
+
+
   return (
     <div
       className={`create-comic-container ${
@@ -21,6 +50,8 @@ const CreateComic = () => {
       }`}
     >
       {' '}
+
+
     </div>
   )
 }
