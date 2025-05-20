692b1da657c1377c05b499c2e278983af7e391b7
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:24:48 +0300

    Added GenerateButton component for comic generation functionality

---
+++ frontend/src/components/ComicCreation/GenerateButton/GenerateButton.jsx
@@ -0,0 +1,16 @@
+import { Icon } from '@iconify-icon/react'
+import PropTypes from 'prop-types'
+import Spinner from '../../../components/Spinner/Spinner'
+import './GenerateButton.css'
+
+const GenerateButton = ({ loading, isGenerating, prompt, onGenerate }) => {
+  return (
+    <div className='generate-btn-container'>
+
+    </div>
+  )
+}
+
+
+
+export default GenerateButton
