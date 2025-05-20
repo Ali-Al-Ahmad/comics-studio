b94cc73fdd69c13f27da91b3b6b24658273fe5e5
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:22:32 +0300

    Added ComicForm component with initial structure and styles

---
+++ frontend/src/components/ComicCreation/ComicForm/ComicForm.jsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types'
+import './ComicForm.css'
+
+const ComicForm = ({ comic, loading, onPromptChange, onStyleChange }) => {
+  return <div className='prompt-input-container'></div>
+}
+
+export default ComicForm
