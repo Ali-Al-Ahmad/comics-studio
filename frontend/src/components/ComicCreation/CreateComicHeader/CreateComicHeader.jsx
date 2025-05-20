00291d7d81aa460bc2445a2749706177d17def95
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-05-20 13:23:52 +0300

    Added CreateComicHeader component for comic creation header

---
+++ frontend/src/components/ComicCreation/CreateComicHeader/CreateComicHeader.jsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types'
+import './CreateComicHeader.css'
+
+const CreateComicHeader = ({ credits }) => {
+  return <div className='create-comic-header'></div>
+}
+
+export default CreateComicHeader
